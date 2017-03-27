af2d9edb09ab1c60803f2bc2c2b208ed5b2ba262
Scott Hyndman <shyndman@google.com>
2017-03-27 11:10:57 -0700

    Adds a reporter class for pretty printing progress.

---
+++ scripts/lib/reporter.js
@@ -0,0 +1,43 @@
+const chalk = require('chalk');
+
+
+class Reporter {
+  constructor() {
+    this.inStep_ = false;
+    this.loggedInStep_ = false;
+  }
+
+  step(stepLabel, runStep, { noCheck } = { noCheck: false }) {
+    this.log_(chalk.cyan(stepLabel) + (noCheck ? '\n' : ''));
+    this.inStep_ = true;
+
+    try {
+      const result = runStep();
+      this.logLine_(this.loggedInStep_ || noCheck ? null : chalk.green('✓'));
+      return result;
+    } catch (e) {
+      throw e;
+    } finally {
+      this.loggedInStep_ = false;
+      this.inStep_ = false;
+    }
+  }
+
+  complete() {
+    this.logLine_(chalk.green('Done!'));
+  }
+
+  log_(msg) {
+    process.stderr.write(msg);
+    if (this.inStep_) {
+      this.loggedInStep_ = true;
+    }
+  }
+
+  logLine_(msg) {
+    this.log_(`${msg || ''}\n`);
+  }
+}
+
+
+module.exports = new Reporter();
