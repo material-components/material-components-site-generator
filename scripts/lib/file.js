3d5bc5455ce8744bdf561cd6dc9dbd65e24eb85e
Scott Hyndman <shyndman@google.com>
2017-04-04 20:16:51 -0400

    Adds code for rewriting (some) Markdown links.

--- scripts/lib/file.js
+++ scripts/lib/file.js
@@ -82,11 +82,16 @@ class JekyllFile extends VinylFile {
           .replace(/-->/, FRONT_MATTER_DELIMITER)
     }
 
-    // Note that [^] matches any character including newlines, whereas "."
-    // does not. For anyone who's wondering, [^] the negation of the empty
-    // set.
-    this.stringContents =
-        stringContents.replace(/<!--([{<][^]*?[>}])-->/g, '$1');
+    this.stringContents = stringContents
+        // Note that [^] matches any character including newlines, whereas "."
+        // does not. For anyone who's wondering, [^] the negation of the empty
+        // set.
+        .replace(/<!--([{<][^]*?[>}])-->/g, '$1')
+        // Rewrite links that point to markdown files to point to html files
+        // instead.
+        .replace(/\[([^\]]+)\]\((.*)\.md\)/g, (match, p1, p2) => {
+          return /^https?:\/\//.test(p2) ? match : `[${p1}](${p2}.html)`;
+        });
   }
 
   /**
