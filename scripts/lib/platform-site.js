8c85da14416c60741a4baf96d1c3aeff0f03314a
Scott Hyndman <shyndman@google.com>
2017-04-18 21:18:04 -0700

    Merges processor functions into the PlatformSite class.

--- scripts/lib/platform-site.js
+++ scripts/lib/platform-site.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const yaml = require('js-yaml');
 const { JekyllFile } = require('./jekyll-file');
-const { PLATFORM_CONFIG_PATH, FilePattern } = require('./project-paths');
+const { PLATFORM_CONFIG_PATH, BuildDir, FilePattern } = require('./project-paths');
 const { sync: globSync } = require('glob');
 
 
@@ -33,6 +33,48 @@ class PlatformSite {
 
   get directoryPaths() {
     return globSync(path.join(this.repoPath, FilePattern.DOCS_DIRS), GLOB_OPTIONS);
+  }
+
+  prepareForBuild() {
+    this.files.forEach((file) => this.processFile_(file));
+    this.directoryPaths.forEach((path) => this.processDocsDirectory_(path));
+  }
+
+  processFile_(file) {
+    if (!file.isValidJekyll) {
+      return false;
+    }
+
+    file.uncommentHiddenCode();
+
+    const fileMetadata = file.jekyllMetadata;
+    if (fileMetadata.path) {
+      this.applyPathRemapping_(file, fileMetadata.path);
+    } else if (file.shouldBecomeIndex) {
+      file.basename = 'index.md';
+    }
+
+    file.path = path.resolve(path.join(BuildDir.STAGE, this.basepath), file.relative);
+    file.base = BuildDir.STAGE;
+    file.write();
+  }
+
+  applyPathRemapping_(file, destPath) {
+    if (destPath.endsWith('/')) {
+      destPath += 'index.md';
+    } else if (destPath.endsWith('.html')) {
+      destPath = destPath.replace(/(.*)\.html$/, '$1.md');
+    }
+
+    file.base = '/';
+    file.path = destPath;
+  }
+
+  processDocsDirectory_(docsDirPath) {
+    const relativePath = path.relative(this.repoPath, docsDirPath);
+    const newPath = path.resolve(path.join(BuildDir.STAGE, this.basepath), relativePath);
+    fs.ensureDirSync(newPath);
+    fs.copySync(docsDirPath, newPath);
   }
 }
 
