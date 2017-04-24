a73613cf88a7cb6cee5cda51d9225c5d9682e5b0
Scott Hyndman <shyndman@google.com>
2017-04-24 10:01:34 -0700

    Adds anchor scroll correction.
    So the linked element doesn't appear beneath the toolbar.

---
+++ jekyll-site-src/_js_src/anchor-scroll.js
@@ -0,0 +1,38 @@
+/**
+ * Initializes anchor scroll correction. This
+ */
+export function initAnchorScrollCorrection(toolbarSelector) {
+  window.addEventListener('DOMContentLoaded', () => {
+    scrollToAnchor(window.location.hash, toolbarSelector);
+  });
+
+  window.addEventListener('hashchange', (e) => {
+    if (scrollToAnchor(window.location.hash, toolbarSelector)) {
+      e.preventDefault();
+    }
+  });
+}
+
+function scrollToAnchor(anchor, toolbarSelector) {
+  if (anchor[0] != '#') {
+    return false;
+  }
+
+  const element = document.querySelector(anchor);
+  if (!element) {
+    return false;
+  }
+
+  const toolbarElement = document.querySelector(toolbarSelector);
+  if (!toolbarElement) {
+    return false;
+  }
+
+  const elementRelativeTop = element.getBoundingClientRect().top;
+  const toolbarHeight = toolbarElement.offsetHeight;
+  window.scroll(
+      window.pageXOffset,
+      window.pageYOffset + elementRelativeTop - toolbarHeight);
+
+  return true;
+}
--- jekyll-site-src/_js_src/index.js
+++ jekyll-site-src/_js_src/index.js
@@ -1,7 +1,9 @@
 import './mdc-code-render';
+import { initAnchorScrollCorrection } from './anchor-scroll';
 import { addHiddenMarks } from './hidden-marks';
 import { initWelcome } from './welcome';
 
 
 initWelcome();
+initAnchorScrollCorrection('.mdc-toolbar');
 addHiddenMarks();
