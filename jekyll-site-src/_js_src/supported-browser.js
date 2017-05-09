48b426187e3d86b822b00cac6c62bd37d173495b
Scott Hyndman <shyndman@google.com>
2017-05-08 20:16:53 -0700

    Removes a word from the unsupported browser screen.

--- jekyll-site-src/_js_src/supported-browser.js
+++ jekyll-site-src/_js_src/supported-browser.js
@@ -6,7 +6,7 @@ export function displayUnsupportedMessage() {
   document.body.classList.add('page');
   document.body.classList.add('page--unsupported-browser');
   document.body.innerHTML = `
-    <h1>Material Design Components</h1>
+    <h1>Material Components</h1>
     <p>
       Oops! Your browser is not yet supported. Visit
       <a href="https://github.com/material-components/">GitHub</a> for more information,
